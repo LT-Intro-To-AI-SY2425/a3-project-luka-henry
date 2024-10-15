e664bb8d09b2aae1a9b80e3ba8412e7f3d2fb281
HenryKlinck1 <hklinck@cps.edu>
2024-10-15 10:35:40 -0500

    ideas for functions if we make own database

--- project.py
+++ project.py
@@ -1,2 +1,15 @@
 from match import match
 from typing import List, Tuple, Callable, Any
+
+def get_recipe():
+def get_ingredient():
+def get_time():
+
+def recipe_by_ingredient():
+    #takes input of ingredient and returns a list of recipes with that ingredient
+def recipe_by_time():
+    #takes input of how long a recipe will take and returns recipies with the same prep time
+def recipe_by_time_range():
+    #takes an input two prep times and returns all recipies that take less than the max and more than the minimum
+def recipe_before_time():
+    #takes an input of prep time and returns all recipies that take less time to prep
