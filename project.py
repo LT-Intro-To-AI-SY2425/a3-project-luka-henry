d52ef6bf33fc34bbf282d9a78c51034db7b21b08
LukaThompson <lbthompson@cps.edu>
2024-10-17 10:03:09 -0500

    addd

--- project.py
+++ project.py
@@ -1,8 +1,19 @@
 from match import match
 from typing import List, Tuple, Callable, Any
-<<<<<<< HEAD
 import urllib.request
 import requests
+def get_recipe():
+def get_ingredient():
+def get_time():
+
+def recipe_by_ingredient():
+    #takes input of ingredient and returns a list of recipes with that ingredient
+def recipe_by_time():
+    #takes input of how long a recipe will take and returns recipies with the same prep time
+def recipe_by_time_range():
+    #takes an input two prep times and returns all recipies that take less than the max and more than the minimum
+def recipe_before_time():
+    #takes an input of prep time and returns all recipies that take less time to prep
 
 # Replace with your PHP URL
 # url = 'https://www.themealdb.com/api/json/v1/1/random.php'
@@ -39,18 +50,3 @@ import requests
 # myfile = f.read()
 # print(myfile)
 
-=======
-
-def get_recipe():
-def get_ingredient():
-def get_time():
-
-def recipe_by_ingredient():
-    #takes input of ingredient and returns a list of recipes with that ingredient
-def recipe_by_time():
-    #takes input of how long a recipe will take and returns recipies with the same prep time
-def recipe_by_time_range():
-    #takes an input two prep times and returns all recipies that take less than the max and more than the minimum
-def recipe_before_time():
-    #takes an input of prep time and returns all recipies that take less time to prep
->>>>>>> e664bb8d09b2aae1a9b80e3ba8412e7f3d2fb281
