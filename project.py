f894ea2d0e0c1589f13e3682ce9d0afdd684e648
HenryKlinck1 <hklinck@cps.edu>
2024-10-18 11:00:21 -0500

    smol change

--- project.py
+++ project.py
@@ -1,7 +1,5 @@
 from match import match
 from typing import List, Tuple, Callable, Any
-import urllib.request
-import requests
 
 def get_name(meal: Tuple[str, str, str, str, List[str]]) -> str:
     return meal[0]
@@ -57,39 +55,4 @@ def recipe_by_location(matches: List[str]) -> List[str]:
             result.append(get_recipe (meal))
     return result
 pass
-
-# Replace with your PHP URL
-# url = 'https://www.themealdb.com/api/json/v1/1/random.php'
-
-# # Send a GET request
-# response = requests.get(url)
-
-# # Check if the request was successful
-# if response.status_code == 200:
-#     # Print the response content
-#     print(response.text)
-# else:
-#     print(f"Error: {response.status_code}")
-
-# def query_loop() -> None:
-#     """The simple query loop. The try/except structure is to catch Ctrl-C or Ctrl-D
-#     characters and exit gracefully.
-#     """
-#     print("Welcome to the cooking database!\n")
-#     while True:
-#         try:
-#             print()
-#             query = input("Your query? ").replace("?", "").lower().split()
-#             answers = search_pa_list(query)
-#             for ans in answers:
-#                 print(ans)
-
-#         except (KeyboardInterrupt, EOFError):
-#             break
-
-#     print("\nSo long!\n")
-# link = ""
-# f = urllib.request.urlopen(link)
-# myfile = f.read()
-# print(myfile)
 
